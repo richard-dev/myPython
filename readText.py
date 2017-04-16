f6228dbf1e63885bab92625afb193bc21393d323
richard-dev <richardl890@gmail.com>
2017-04-16 09:02:36 -0400

    Profanity Checker complete.

--- readText.py
+++ readText.py
@@ -9,9 +9,17 @@ def readText():
 
 def checkProfanity(textToCheck):
     query = urllib.parse.urlencode({'q': textToCheck})
+    print("\n\n" + query + "\n\n")
     conn = urllib.request.urlopen("http://www.wdylike.appspot.com/?q="+query)
-    output = conn.read()
-    print(output)
+    output = conn.read().decode("utf-8")
+    print("\n" + output + "\n\n")
     conn.close()
+
+    if "true" in output:
+        print("Profanity Alert!")
+    elif "false" in output:
+        print("This document has no curse words!")
+    else:
+        print("Could not scan document properly.")
 
 readText()
