028202a3e298654ed59c1166e56472dd9ba3d29d
richard-dev <richardl890@gmail.com>
2017-04-18 16:49:56 -0400

    Final commit inheritence.

--- inheritence/parent.py
+++ inheritence/parent.py
@@ -4,12 +4,23 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name: " + self.last_name)
+        print("Eye Color: " + self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child constructor called")
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
-CASSIE = Child("Leung", "brown", 5)
-print(CASSIE.last_name)
-print(CASSIE.number_of_toys)
+    def show_info(self):
+        print("Last Name: " + self.last_name)
+        print("Eye Color: " + self.eye_color)
+        print("Number of Toys: " + str(self.number_of_toys))
+
+# JEILY = Parent("Leung", "brown")
+# print(JEILY.show_info())
+
+CASSIE = Child("Leung", "blue", 5)
+print(CASSIE.show_info())
